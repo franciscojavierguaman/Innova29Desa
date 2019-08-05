93cdd54741b07bc3143cca2ebaad2525892a3685
franciscojavierguaman <fguaman@29deoctubre.fin.ec>
2019-08-05 12:02:42 -0500

    Agregar archivos de proyecto.

---
+++ Innova29.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Innova29.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Innova29/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Innova29.MvcApplication" Language="C#" %>
